f749c9d4a4617b95bad7feff0b904a26a8a61fe5
Kennedy <kiplangatkennedy019@gmail.com>
2023-06-24 05:46:03 +0300

    completed the assignment

--- server/app.py
+++ server/app.py
@@ -17,10 +17,35 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict =[n.to_dict() for n in Plant.query.all()]
+    
+        response = make_response(
+
+             jsonify(response_dict),
+             200,
+         )
+    
+        return response
+
+api.add_resource(Plants, '/plants')
+
+    
 
 class PlantByID(Resource):
-    pass
+    def get(self ,id):
+
+        response_dict = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            200,
+        )
+        return response
+    
+api.add_resource(PlantByID,'/plants/<int:id>')
+        
+    
         
 
 if __name__ == '__main__':
